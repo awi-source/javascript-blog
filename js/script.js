1410c415a8328ab7789f28e77250e8b176a0f99a
Agnieszka Witek <witek.aga@gmail.com>
2020-10-11 09:41:30 +0200

    Remove titlelist

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-'use strict'
+{
+  'use strict'
 
   const titleClickHandler = function(event){
     event.preventDefault();
@@ -37,3 +38,31 @@
   for(let link of links){
     link.addEventListener('click', titleClickHandler);
   }
+
+  const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
+function generateTitleLinks(){
+
+  /* [DONE] remove contents of titleList */
+const titlelist = document.querySelector(optTitleListSelector);
+console.log(titlelist);
+titlelist.innerHTML='';
+  /* [IN PROGRESS] for each article */
+
+    /* get the article id */
+
+    /* find the title element */
+
+    /* get the title from the title element */
+
+    /* create HTML of the link */
+
+    /* insert link into titleList */
+
+}
+
+generateTitleLinks();
+
+}
