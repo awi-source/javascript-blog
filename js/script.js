d6099e4e55abae8407a94ee9a2745c717015a2dd
Agnieszka Witek <witek.aga@gmail.com>
2020-10-13 07:37:01 +0200

    Clear comments

--- js/script.js
+++ js/script.js
@@ -33,43 +33,33 @@
     targetArticle.classList.add('active');
   }
   
- /*const links = document.querySelectorAll('.titles a');
-  console.log(links);
-  for(let link of links){
-  link.addEventListener('click', titleClickHandler);
-  }*/
-
   const optArticleSelector = '.post',
   optTitleSelector = '.post-title',
   optTitleListSelector = '.titles';
 
 function generateTitleLinks(){
 console.log(generateTitleLinks);
-  /* [DONE] remove contents of titleList */
 const titleList = document.querySelector(optTitleListSelector);
 console.log(titleList);
 
- /* [DONE] for each article */
  let html = '';
 const articles = document.querySelectorAll(optArticleSelector);
 for(let article of articles){
  console.log(article);
- /* [DONE] get the article id */ 
+
   const articleId = article.getAttribute('id');
   console.log(articleId);
-   /* [DONE] find the title element *//* [DONE] get the title from the title element */
+  
  
   const articleTitle = article.querySelector(optTitleSelector).innerHTML;
   console.log(articleTitle);
    
-/* [DONE] create HTML of the link */
+
  const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
   console.log(linkHTML);
-/*[DONE] insert link into titleList */
-//titleList.innerHTML = titleList.innerHTML + linkHTML;  
+ 
 html = html + linkHTML;
-//titleList.innerHTML = html;
-//console.log(html);
+
 }
 titleList.innerHTML = html;
 console.log(html);
